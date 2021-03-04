e94e53951cfab21568fd5f241f1a2e686cbabcfa
VincLee8188 <37565227+VincLee8188@users.noreply.github.com>
2021-03-04 17:28:41 +0800

    add wgan

--- wgan.py
+++ wgan.py
@@ -1,212 +1,339 @@
 import torch
-from torch import nn, optim, autograd
-import numpy as np
-import random
-from torch.nn import functional as F
-import visdom
+from torch import nn
+from tqdm import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+torch.manual_seed(0)
 
-EPOCH = 10000
-h_dim = 400
-batch_size = 512
-viz = visdom.Visdom()
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in an uniform grid.
+    '''
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
+
+
+def make_grad_hook():
+    '''
+    Function to keep track of gradients for visualization purposes,
+    which fills the grads list when using model.apply(grad_hook).
+    '''
+    grads = []
+    def grad_hook(m):
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+            grads.append(m.weight.grad)
+    return grads, grad_hook
+
+
+def get_gradient(crit, real, fake, epsilon):
+    '''
+    Return the gradient of the critic's scores with respect to mixes of real and fake images.
+    Parameters:
+        crit: the critic model
+        real: a batch of real images
+        fake: a batch of fake images
+        epsilon: a vector of the uniformly random proportions of real/fake per mixed image
+    Returns:
+        gradient: the gradient of the critic's scores, with respect to the mixed image
+    '''
+    # Mix the images together
+    mixed_images = real * epsilon + fake * (1 - epsilon)
+
+    # Calculate the critic's scores on the mixed images
+    mixed_scores = crit(mixed_images)
+
+    # Take the gradient of the scores with respect to the images
+    gradient = torch.autograd.grad(
+        # Note: You need to take the gradient of outputs with respect to inputs.
+        # This documentation may be useful, but it should not be necessary:
+        # https://pytorch.org/docs/stable/autograd.html#torch.autograd.grad
+        inputs=mixed_images,
+        outputs=mixed_scores,
+        # These other parameters have to do with the pytorch autograd engine works
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    return gradient
+
+
+def gradient_penalty(gradient):
+    '''
+    Return the gradient penalty, given a gradient.
+    Given a batch of image gradients, you calculate the magnitude of each image's gradient
+    and penalize the mean quadratic distance of each magnitude to 1.
+    Parameters:
+        gradient: the gradient of the critic's scores, with respect to the mixed image
+    Returns:
+        penalty: the gradient penalty
+    '''
+    # Flatten the gradients so that each row captures one image
+    gradient = gradient.view(len(gradient), -1)
+
+    # Calculate the magnitude of every row
+    gradient_norm = gradient.norm(2, dim=1)
+
+    # Penalize the mean squared distance of the gradient norms from 1
+    penalty = torch.mean((gradient_norm - 1) ** 2)
+    return penalty
 
 
 class Generator(nn.Module):
-
-    def __init__(self):
+    '''
+    Generator Class
+    Values:
+        z_dim: the dimension of the noise vector, a scalar
+        im_chan: the number of channels in the images, fitted for the dataset used, a scalar
+              (MNIST is black-and-white, so 1 channel is your default)
+        hidden_dim: the inner dimension, a scalar
+    '''
+    def __init__(self, z_dim=10, im_chan=1, hidden_dim=64):
         super(Generator, self).__init__()
-
-        self.net = nn.Sequential(
-            nn.Linear(2, h_dim),
-            nn.ReLU(True),
-            nn.Linear(h_dim, h_dim),
-            nn.ReLU(True),
-            nn.Linear(h_dim, h_dim),
-            nn.ReLU(True),
-            nn.Linear(h_dim, 2)
+        self.z_dim = z_dim
+        # Build the neural network
+        self.gen = nn.Sequential(
+            self.make_gen_block(z_dim, hidden_dim * 4),
+            self.make_gen_block(hidden_dim * 4, hidden_dim * 2, kernel_size=4, stride=1),
+            self.make_gen_block(hidden_dim * 2, hidden_dim),
+            self.make_gen_block(hidden_dim, im_chan, kernel_size=4, final_layer=True),
         )
 
-    def forward(self, z):
-        output = self.net(z)
-        return output
+    def make_gen_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a generator block of DCGAN;
+        a transposed convolution, a batchnorm (except in the final layer), and an activation.
+        Parameters:
+            input_channels: how many channels the input feature representation has
+            output_channels: how many channels the output feature representation should have
+            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+            stride: the stride of the convolution
+            final_layer: a boolean, true if it is the final layer and false otherwise
+                      (affects activation and batchnorm)
+        '''
+        if not final_layer:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.ReLU(inplace=True),
+            )
+        else:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.Tanh(),
+            )
+
+    def forward(self, noise):
+        '''
+        Function for completing a forward pass of the generator: Given a noise tensor,
+        returns generated images.
+        Parameters:
+            noise: a noise tensor with dimensions (n_samples, z_dim)
+        '''
+        x = noise.view(len(noise), self.z_dim, 1, 1)
+        return self.gen(x)
 
 
-class Discriminator(nn.Module):
+def get_noise(n_samples, z_dim, device='cpu'):
+    '''
+    Function for creating noise vectors: Given the dimensions (n_samples, z_dim)
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+      n_samples: the number of samples to generate, a scalar
+      z_dim: the dimension of the noise vector, a scalar
+      device: the device type
+    '''
+    return torch.randn(n_samples, z_dim, device=device)
 
-    def __init__(self):
-        super(Discriminator, self).__init__()
 
-        self.net = nn.Sequential(
-            nn.Linear(2, h_dim),
-            nn.ReLU(True),
-            nn.Linear(h_dim, h_dim),
-            nn.ReLU(True),
-            nn.Linear(h_dim, h_dim),
-            nn.ReLU(True),
-            nn.Linear(h_dim, 1),
-            nn.Sigmoid()
+class Critic(nn.Module):
+    '''
+    Critic Class
+    Values:
+        im_chan: the number of channels in the images, fitted for the dataset used, a scalar
+              (MNIST is black-and-white, so 1 channel is your default)
+        hidden_dim: the inner dimension, a scalar
+    '''
+    def __init__(self, im_chan=1, hidden_dim=64):
+        super(Critic, self).__init__()
+        self.crit = nn.Sequential(
+            self.make_crit_block(im_chan, hidden_dim),
+            self.make_crit_block(hidden_dim, hidden_dim * 2),
+            self.make_crit_block(hidden_dim * 2, 1, final_layer=True),
         )
 
-    def forward(self, x):
-        output = self.net(x)
-        return output.view(-1)
+    def make_crit_block(self, input_channels, output_channels, kernel_size=4, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a critic block of DCGAN;
+        a convolution, a batchnorm (except in the final layer), and an activation (except in the final layer).
+        Parameters:
+            input_channels: how many channels the input feature representation has
+            output_channels: how many channels the output feature representation should have
+            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+            stride: the stride of the convolution
+            final_layer: a boolean, true if it is the final layer and false otherwise
+                      (affects activation and batchnorm)
+        '''
+        if not final_layer:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.LeakyReLU(0.2, inplace=True),
+            )
+        else:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride),
+            )
+
+    def forward(self, image):
+        '''
+        Function for completing a forward pass of the critic: Given an image tensor,
+        returns a 1-dimension tensor representing fake/real.
+        Parameters:
+            image: a flattened image tensor with dimension (im_chan)
+        '''
+        crit_pred = self.crit(image)
+        return crit_pred.view(len(crit_pred), -1)
 
 
-def data_generator():
-    """
-    8-gaussian mixture models
-    :return:
-    """
-
-    scale = 2.
-    centers = [
-        (1, 0),
-        (-1, 0),
-        (0, 1),
-        (0, -1),
-        (1. / np.sqrt(2), 1. / np.sqrt(2)),
-        (1. / np.sqrt(2), -1. / np.sqrt(2)),
-        (-1. / np.sqrt(2), 1. / np.sqrt(2)),
-        (-1. / np.sqrt(2), -1. / np.sqrt(2)),
-    ]
-    centers = [(scale * x, scale * y) for x, y in centers]
-
-    while True:
-        dataset = []
-        for i in range(batch_size):
-            point = np.random.randn(2) * 0.02
-            center = random.choice(centers)
-            point[0] += center[0]
-            point[1] += center[1]
-            dataset.append(point)
-        dataset = np.array(dataset).astype(np.float32)
-        dataset /= 1.1415
-        yield dataset
+def get_gen_loss(crit_fake_pred):
+    '''
+    Return the loss of a generator given the critic's scores of the generator's fake images.
+    Parameters:
+        crit_fake_pred: the critic's scores of the fake images
+    Returns:
+        gen_loss: a scalar loss value for the current batch of the generator
+    '''
+    gen_loss = - torch.mean(crit_fake_pred)
+    return gen_loss
 
 
-def weights_init(m):
-    if isinstance(m, nn.Linear):
-        # m.weight.data.normal_(0.0, 0.02)
-        nn.init.kaiming_normal_(m.weight)
-        m.bias.data.fill_(0)
-
-
-def gradient_penalty(D, xr, xf):
-    LAMBDA = 0.3
-
-    # only constrained for discriminator
-    xf = xf.detach()
-    xr = xr.detach()
-
-    alpha = torch.rand(batch_size, 1).to(device)
-    alpha = alpha.expand_as(xr)
-
-    interpolates = alpha * xr + ((1 - alpha) * xf)
-    interpolates.requires_grad_()
-    disc_interpolates = D(interpolates)
-    gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
-                              grad_outputs=torch.ones_like(disc_interpolates),
-                              create_graph=True, retain_graph=True, only_inputs=True)[0]
-    gp = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * LAMBDA
-    return gp
-
-
-def generate_image(D, G, xr, epoch):
-    """
-    Generates and saves a plot of the true distribution, the generator, and the
-    critic.
-    """
-    N_POINTS = 128
-    RANGE = 3
-    plt.clf()
-
-    points = np.zeros((N_POINTS, N_POINTS, 2), dtype='float32')
-    points[:, :, 0] = np.linspace(-RANGE, RANGE, N_POINTS)[:, None]
-    points[:, :, 1] = np.linspace(-RANGE, RANGE, N_POINTS)[None, :]
-    points = points.reshape((-1, 2))
-    # (16384, 2)
-    # print('p:', points.shape)
-
-    # draw contour
-    with torch.no_grad():
-        points = torch.Tensor(points).to(device)  # [16384, 2]
-        disc_map = D(points).cpu().numpy()  # [16384]
-    x = y = np.linspace(-RANGE, RANGE, N_POINTS)
-    cs = plt.contour(x, y, disc_map.reshape((len(x), len(y))).transpose())
-    plt.clabel(cs, inline=1, fontsize=10)
-    # plt.colorbar()
-
-    # draw samples
-    with torch.no_grad():
-        z = torch.randn(batch_size, 2).to(device)  # [b, 2]
-        samples = G(z).cpu()  # [b, 2]
-    plt.scatter(xr[:, 0], xr[:, 1], c='orange', marker='.')
-    plt.scatter(samples[:, 0], samples[:, 1], c='green', marker='+')
-
-    viz.matplot(plt, win='contour', opts=dict(title='p(x):%d' % epoch))
-
-
-def main():
-    torch.manual_seed(2333)
-    np.random.seed(2333)
-    random.seed(2333)
-
-    data_iter = data_generator()
-    # x = next(data_iter)
-    # [b, 2]
-    # print(x.shape)
-
-    G = Generator().to(device)
-    D = Discriminator().to(device)
-    # G.apply(weights_init)
-    # D.apply(weights_init)
-    # print(G)
-    # print(D)
-    optim_G = optim.Adam(G.parameters(), lr=5e-4, betas=(0.5, 0.9))
-    optim_D = optim.Adam(D.parameters(), lr=5e-4, betas=(0.5, 0.9))
-    viz.line([[0, 0]], [0], win='loss', opts=dict(title='loss', legend=['D', 'G']))
-
-    for epoch in range(EPOCH):
-
-        # 1. train Discriminator
-        for _ in range(5):
-            # 1.1. train on real data
-            x = next(data_iter)
-            xr = torch.from_numpy(x).to(device)
-            predr = D(xr)
-            lossr = -(predr.mean())
-
-            # 1.2. train on generated data
-            z = torch.randn(batch_size, 2).to(device)
-            predg = D(G(z).detach())
-            lossg = predg.mean()
-
-            # 1.3. add gradient penalty
-            gdp = gradient_penalty(D, xr, G(z))
-
-            # aggregate all
-            loss_D = lossr + lossg + gdp
-
-            # optimize
-            optim_D.zero_grad()
-            loss_D.backward()
-            optim_D.step()
-
-        # 2. train Generator
-        z = torch.randn(batch_size, 2).to(device)
-        loss_G = -D(G(z)).mean()
-
-        optim_G.zero_grad()
-        loss_G.backward()
-        optim_G.step()
-
-        if epoch % 50 == 0:
-            viz.line([[loss_D.item(), loss_G.item()]], [epoch], win='loss', update='append')
-            generate_image(D, G, xr.cpu(), epoch)
-            print('\nEpoch: {:3}/{}, loss_G: {:.4f}, loss_D: {:.4f}'.format(epoch + 50, EPOCH, loss_G, loss_D))
+def get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda):
+    '''
+    Return the loss of a critic given the critic's scores for fake and real images,
+    the gradient penalty, and gradient penalty weight.
+    Parameters:
+        crit_fake_pred: the critic's scores of the fake images
+        crit_real_pred: the critic's scores of the real images
+        gp: the unweighted gradient penalty
+        c_lambda: the current weight of the gradient penalty
+    Returns:
+        crit_loss: a scalar for the critic's loss, accounting for the relevant factors
+    '''
+    crit_loss = torch.mean(crit_fake_pred - crit_real_pred) + c_lambda*gp
+    return crit_loss
 
 
 if __name__ == '__main__':
-    main()
+    n_epochs = 100
+    z_dim = 64
+    display_step = 50
+    batch_size = 128
+    lr = 0.0002
+    beta_1 = 0.5
+    beta_2 = 0.999
+    c_lambda = 10
+    crit_repeats = 5
+    device = 'cuda'
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,)),
+    ])
+
+    dataloader = DataLoader(
+        MNIST('.', download=False, transform=transform),
+        batch_size=batch_size,
+        shuffle=True)
+
+    gen = Generator(z_dim).to(device)
+    gen_opt = torch.optim.Adam(gen.parameters(), lr=lr, betas=(beta_1, beta_2))
+    crit = Critic().to(device)
+    crit_opt = torch.optim.Adam(crit.parameters(), lr=lr, betas=(beta_1, beta_2))
+
+    def weights_init(m):
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+            torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        if isinstance(m, nn.BatchNorm2d):
+            torch.nn.init.normal_(m.weight, 0.0, 0.02)
+            torch.nn.init.constant_(m.bias, 0)
+    gen = gen.apply(weights_init)
+    crit = crit.apply(weights_init)
+
+    cur_step = 0
+    generator_losses = []
+    critic_losses = []
+    for epoch in range(n_epochs):
+        # Dataloader returns the batches
+        for real, _ in tqdm(dataloader):
+            cur_batch_size = len(real)
+            real = real.to(device)
+
+            mean_iteration_critic_loss = 0
+            for _ in range(crit_repeats):
+                ### Update critic ###
+                crit_opt.zero_grad()
+                fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+                fake = gen(fake_noise)
+                crit_fake_pred = crit(fake.detach())
+                crit_real_pred = crit(real)
+
+                epsilon = torch.rand(len(real), 1, 1, 1, device=device, requires_grad=True)
+                gradient = get_gradient(crit, real, fake.detach(), epsilon)
+                gp = gradient_penalty(gradient)
+                crit_loss = get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda)
+
+                # Keep track of the average critic loss in this batch
+                mean_iteration_critic_loss += crit_loss.item() / crit_repeats
+                # Update gradients
+                crit_loss.backward(retain_graph=True)
+                # Update optimizer
+                crit_opt.step()
+            critic_losses += [mean_iteration_critic_loss]
+
+            ### Update generator ###
+            gen_opt.zero_grad()
+            fake_noise_2 = get_noise(cur_batch_size, z_dim, device=device)
+            fake_2 = gen(fake_noise_2)
+            crit_fake_pred = crit(fake_2)
+
+            gen_loss = get_gen_loss(crit_fake_pred)
+            gen_loss.backward()
+
+            # Update the weights
+            gen_opt.step()
+
+            # Keep track of the average generator loss
+            generator_losses += [gen_loss.item()]
+
+            ### Visualization code ###
+            if cur_step % display_step == 0 and cur_step > 0:
+                gen_mean = sum(generator_losses[-display_step:]) / display_step
+                crit_mean = sum(critic_losses[-display_step:]) / display_step
+                print(f"Step {cur_step}: Generator loss: {gen_mean}, critic loss: {crit_mean}")
+                show_tensor_images(fake)
+                show_tensor_images(real)
+                step_bins = 20
+                num_examples = (len(generator_losses) // step_bins) * step_bins
+                plt.plot(
+                    range(num_examples // step_bins),
+                    torch.Tensor(generator_losses[:num_examples]).view(-1, step_bins).mean(1),
+                    label="Generator Loss"
+                )
+                plt.plot(
+                    range(num_examples // step_bins),
+                    torch.Tensor(critic_losses[:num_examples]).view(-1, step_bins).mean(1),
+                    label="Critic Loss"
+                )
+                plt.legend()
+                plt.show()
+
+            cur_step += 1
+
